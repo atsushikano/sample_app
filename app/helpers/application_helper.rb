2080b262bbdab0ae28e60291ac5a1f20519a4242
狩野惇 <ash.surfing.life@gmail.com>
2018-01-23 00:29:25 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    if page_title.empty?
+      base_title
+    end
+      base_title + " | " + base_title
+  end
 end
