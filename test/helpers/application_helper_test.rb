923d5c9196aec72cd7a56e919d18d63a4949c20e
狩野惇 <ash.surfing.life@gmail.com>
2018-01-25 23:03:49 +0900

    Finish user signup

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,8 +1,12 @@
-require 'test_helper'
 
-class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
-    assert_equal full_title,         FILL_IN
-    assert_equal full_title("Help"), FILL_IN
+module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
   end
 end
